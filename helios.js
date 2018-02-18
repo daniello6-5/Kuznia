138410c78282da5d7e1ae46b74a5fa3ddd739789
daniello6-5 <daniello653@gmail.com>
2018-02-18 11:42:09 +0100

    baza danych

---
+++ helios.js
@@ -0,0 +1,106 @@
+
+{
+   "ulica":"Jurowiecka",
+   "miasto":"Bialystok",
+   "filmy":[
+      {
+         "tytul":"Czarna Pantera",
+         "dlugoscfilmu":"90"
+      },
+      {
+         "tytul":"Transformers 3",
+         "dlugoscfilmu":"75"
+      }
+   ]
+}
+
+
+
+{
+   "ulica":"Milosza 2",
+   "miasto":"Bialystok",
+   "filmy":[
+      {
+         "tytul":"Mumia 2017",
+         "dlugoscfilmu":"95"
+      },
+      {
+         "tytul":"The Post",
+         "dlugoscfilmu":"60"
+      }
+   ]
+}
+
+
+
+{
+   "ulica":"Swietojanska",
+   "miasto":"Bialystok",
+   "filmy":[
+      {
+         "tytul":"Plan B",
+         "dlugoscfilmu":"55"
+      },
+      {
+         "tytul":"Narzeczony na niby",
+         "dlugoscfilmu":"65"
+      }
+   ]
+}
+
+
+{
+   "ulica":"Wilenska",
+   "miasto":"Warszawa",
+   "filmy":[
+      {
+         "tytul":"Pszczolka Maja",
+         "dlugoscfilmu":"65"
+      },
+      {
+         "tytul":"Tomb Raider",
+         "dlugoscfilmu":"65"
+      }
+   ]
+}
+
+
+{
+   "ulica":"1-go Maja",
+   "miasto":"Warszawa",
+   "filmy":[
+      {
+         "tytul":"Pułapka czasu",
+         "dlugoscfilmu":"85"
+      },
+      {
+         "tytul":"Szklanka po lapkach",
+         "dlugoscfilmu":"65"
+      }
+   ]
+}
+
+
+
+
+{
+   "ulica":"Kopernika",
+   "miasto":"Warszawa",
+   "filmy":[
+      {
+         "tytul":"Naga Bron 2",
+         "dlugoscfilmu":"75"
+      },
+      {
+         "tytul":"Terminator 3",
+         "dlugoscfilmu":"95"
+      }
+   ]
+}
+
+
+
+
+
+
+
